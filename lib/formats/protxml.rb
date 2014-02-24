923c8d8c457f38f2103b2ec17513e8e0e72a1962
Vital @ UCM <uveuve@gmail.com>
2014-02-24 19:45:10 +0100

    watch out with getModificationCvParam, especially L 0

--- lib/formats/protxml.rb
+++ lib/formats/protxml.rb
@@ -11,10 +11,10 @@ class ProtXML < PepXML
   # Since the prot.xml file is created from the pepxml file and it uses references to SpectrumIdentificationItem and peptides, it makes sense to make this class inherit from PepXML
   # @param [String] file the location of the protXML file
   # @param [String] database the location of the FASTA database that was used by the search engine
-	
+
   #Vital:
-  attr_reader :file, :database 
-	
+  attr_reader :file, :database
+
   #@protxml_doc = Nokogiri::XML(File.open("SILAC_phos_OrbitrapVelos_1_interact-highprob.prot.xml"))
   # hyph_protxml_obj = ProtXML.new("Hyphal_extract_OrbitrapVelos/XTK_Calb1_0.05_0.05_yes/hyphal_interact-highprob.prot.xml", "C_albicans_SC5314_version_A21-s02-m01-r07_orf_trans_all_plus_contaminants_DECOY.fasta")
   def initialize(file, database)
@@ -23,23 +23,23 @@ class ProtXML < PepXML
     @type = "protxml"
     @doc = Nokogiri::XML(File.open(file))
     @xmlns = ""
-    
+
     # Nokogiri won't parse out the information of an XML file that uses namespaces unless you add xmlns, and vice versa.
     @xmlns = "xmlns:" if hasNamespace
-    
+
   end
 
 
 
   def proteinDetectionParams #Recoge los datos para <AnalysisCollection><ProteinDetection> y para <AnalysisProtocolCollection><ProteinDetectionProtocol>
-  
+
   #<AnalysisCollection> #OJO! En teoría es un ProteinDetection por cada SIL !! Pero creo que en el protXML, estan todas las proteínas detectadas sin referencia a que SIL corresponden
   #<ProteinDetection id="PD_1" proteinDetectionProtocol_ref="PDP_MascotParser_1" proteinDetectionList_ref="PDL_1" activityDate="2011-03-25T13:33:51">
     #<InputSpectrumIdentifications spectrumIdentificationList_ref="SIL_1"/> #minOccurs = 1
   #</ProteinDetection>
-  
+
   #<AnalysisProtocolCollection>
-  
+
    # <ProteinDetectionProtocol id="PDP_MascotParser_1" analysisSoftware_ref="AS_mascot_parser">
      # <AnalysisParams>
       # <cvParam accession="MS:1001316" name="mascot:SigThreshold" cvRef="PSI-MS" value="0.05"/>
@@ -47,9 +47,9 @@ class ProtXML < PepXML
       # <userParam name="proteinprophet:param" value="0.01"
       # ...
    # </ProteinDetectionProtocol>
-   
+
    # ms_run_params_set = MsRunParamsSet.new(Msrunparams.new(inputraw, searchEngine, manufacturer, model, ionization, analyzer, detector, raw_data_file_type))
-    
+
 
     prot_summ_header = @doc.xpath("//xmlns:protein_summary_header")
     ref_db = prot_summ_heaader.xpath("./@reference_database").to_s
@@ -66,34 +66,35 @@ class ProtXML < PepXML
     total_no_spectrum_ids  = protein_summary_header.xpath("./@total_no_spectrum_ids").to_s
     sample_enzyme = protein_summary_header.xpath("./@sample_enzyme").to_s
 
-    #~ protein_detection_params = 
+    #~ protein_detection_params =
 
-  
-  
+
+
     #~ ProteinDetectionParamsSet.new(
-  
+
   end
-  
-  
 
 
-  
+
+
+
 
   def proteinGroups #OJO, igual que para el caso de ProteinDetection y ProteinDetectionProtocol tndria que hacer varias listas, pero no puedo obtener el SIL de referencia!!
-  
+
     all_protein_groups = @doc.xpath("//#{@xmlns}protein_group")
-    
+
     pags = []
     all_protein_groups.each do |group| #uso all_protein_groups[445] porque tiene varias <protein y una de ellas tien 3 indisti_proteins
       group_number = group.xpath("./@group_number").to_s
       group_name = "PAG_#{group_number}"
-      
+
       protein_hypothesis_set = []
-      #protein_hypothesis_set = Array de objetos ProteinHypothesis 
+      #protein_hypothesis_set = Array de objetos ProteinHypothesis
       #ProteinHypothesis contiene protein_name, prot_hypothesis_params, threshold, y peptide_hypothesis_set -> Array de objetos PeptideHypothesis
-            
+
       group.xpath(".//#{@xmlns}protein|.//#{@xmlns}indistinguishable_protein").each do |protein|
         protein_name = protein.xpath("./@protein_name").to_s
+        protein_ID = proteinID(protein.xpath("./@protein_name").to_s)
         protein = protein.parent if protein.node_name == "indistinguishable_protein"
         protein_hypothesis_params = []
         #Estas protein, si no las encuentro en @@prot_acs, no puedo poner el dBSequence_ref en <ProteinHypothesis
@@ -108,19 +109,19 @@ class ProtXML < PepXML
         pct_spectrum_ids = protein.xpath("./@pct_spectrum_ids").to_s
         confidence = protein.xpath("./@confidence").to_s
         if protein.xpath(".//#{@xmlns}parameter/@name").to_s == "prot_length"
-          prot_length = protein.xpath(".//#{@xmlns}parameter/@value").to_s 
+          prot_length = protein.xpath(".//#{@xmlns}parameter/@value").to_s
         else
           prot_length = ""
-        end     
+        end
         prot_hypothesis_params = [dbseq_ref, n_indist_proteins, probability, percent_coverage, unique_stripped_peptides, group_sibling_id, total_number_peptides, pct_spectrum_ids, confidence, prot_length]
-        
-        peptide_hypothesis_set = []        
-        peptides = protein.xpath(".//#{@xmlns}peptide") 
-        #@@peps (pepxml.rb) [["peptide_223_6", ["CFTAGTNTVTFNDGGK", [["C", 1, "57.0215", ["UNIMOD", "UNIMOD:4", "Carbamidomethyl"]]]]]]       
-        
+
+        peptide_hypothesis_set = []
+        peptides = protein.xpath(".//#{@xmlns}peptide")
+        #@@peps (pepxml.rb) [["peptide_223_6", ["CFTAGTNTVTFNDGGK", [["C", 1, "57.0215", ["UNIMOD", "UNIMOD:4", "Carbamidomethyl"]]]]]]
+
         #OJO! CuIDAO CON ESTO:
         #peptide_207_1 -> orf19.2020, orf19.2021 y orf19.2023
- 
+
         pep_ids = []
         peptides.each do |pep| #Los <peptide > de una <protein>
           #Obtener peptide_id de cada uno de estos <peptide> (peptide_id: peptide_439_6")
@@ -130,25 +131,30 @@ class ProtXML < PepXML
             #SI PARA ESTE protxml_pepseq no encuentro su pepxml_pepseq correspondiente NO entrará en este PAG, porque no podré aportar un <PeptideHypothesis con su peptideEvidence_ref !!!
             pepxml_pepseq = pepxml_pep[1][0]
             pepxml_pepmod = pepxml_pep[1][1]
-            if protxml_pepseq == pepxml_pepseq 
+            if protxml_pepseq == pepxml_pepseq
+
+              #OJORL!!.: No tendría que comprobar que las modificaciones tambien son las mismas?!
               if (protxml_pepmod.empty? and pepxml_pepmod.empty?) or !protxml_pepmod.empty? #En el prot.xml, Si un pep "SPSS[..]NLSK" está sin modificar (peptide_46_7) y modificado (peptide_20_10) aparece bajo un solo elemento <peptide> !
-                pep_ids << pepxml_pep[0] 
+
+
+
+                pep_ids << pepxml_pep[0]
               end
             end
           end
         end
-        
+
         #Y luego para cada pep_id saco su(s) pep_Evidence(s) y su(s) sii
         pep_ids.each do |pep_id| #estos son los peptidos de cada proteína
           peptide_hypothesis = {} #peptide_hypothesis["peptide_151_10_SDB_1_orf19.5629_45_61"] = ["SIL_Elu2A_SII_589_1"
           @@peptide_evidences.each do |ev| #["peptide_1_1", "F", "K", "0", "orf19.1086", 554, 569]
-          
-          
-          
-            if pep_id == ev[0] and protein_name == ev[4]
-            
-            
-            
+
+
+
+            if pep_id == ev[0] and protein_ID == ev[4]
+
+
+
               pep_ev = "#{ev[0]}_SDB_1_#{ev[4]}_#{ev[5]}_#{ev[6]}"
               sii_s = []
               @@all_items.each do |sii|
@@ -156,26 +162,26 @@ class ProtXML < PepXML
               end
               #peptide_hypothesis_set << PeptideHypothesis.new(pep_evs, sii_s)
               peptide_hypothesis[pep_ev] = sii_s
-            end            
+            end
           end
           peptide_hypothesis_set << PeptideHypothesis.new(peptide_hypothesis)
-        end       
-   
+        end
+
 #   id: MS:1001098 name: confident distinct peptide sequences cvParam => Lo cojo del numero de PeptideHypothesis
-   
+
         if !peptide_hypothesis_set.empty?
           protein_hypothesis_set << ProteinHypothesis.new(protein_name, n_indist_proteins, pass_threshold, prot_hypothesis_params, peptide_hypothesis_set)
         end
-        
+
       end
-  
+
       pags << ProteinGroup.new(group_name, protein_hypothesis_set) unless protein_hypothesis_set.empty?
-      
+
     end
-    
-    
+
+
     pags
-    
+
           #~ initial_probability = pep.xpath("./@initial_probability").to_s
           #~ nsp_adjusted_probability = pep.xpath("./@nsp_adjusted_probability").to_s
           #~ weight = pep.xpath("./@weight").to_s
@@ -187,12 +193,32 @@ class ProtXML < PepXML
           #~ exp_tot_instances = pep.xpath("./@exp_tot_instances").to_s
           #~ is_contributing_evidence = pep.xpath("./@is_contributing_evidence").to_s
           #~ pep_vals = [initial_probability, nsp_adjusted_probability, weight, is_nondegenerate_evidence, is_nondegenerate_evidence, n_enzymatic_termini, n_sibling_peptides, n_sibling_peptides_bin, n_instances, exp_tot_instances, is_contributing_evidence]
-        
-      
-  
+
+
+
   end
 
-  
+
+
+
+
+  def proteinID(protein)
+    #If a protein ID contains a "|", then it contains more than just the ID
+    if protein.include?('|')
+      arr = protein.split("|")[1].split(":")
+      if arr.length == 1
+        arr[0]
+      else
+        arr[1]
+      end
+    #If there's no characters, then it's an index. I don't fully understand regexp, but this works.
+    elsif (protein =~ /[A-Z]/i) == nil
+      @proteinIndices[protein.to_i]
+    else
+      protein
+    end
+  end
+
 
 
 
--- lib/search2mzidentml.rb
+++ lib/search2mzidentml.rb
@@ -27,7 +27,7 @@ class Search2mzIdentML
     @pepxml = pepxml
     @protxml = protxml
   end
-  
+
   # Starts the Nokogiri build process. Other methods build the different parts of the file. Root is depth 0
   #
   # @option opts [] currently does nothing
@@ -48,7 +48,7 @@ class Search2mzIdentML
           cvList(xml)
           analysisSoftwareList(xml)
           #analysisSampleCollection(xml)
-          provider(xml)  
+          provider(xml)
           auditCollection(xml)
           sequenceCollection(xml)
           analysisCollection(xml)
@@ -56,21 +56,21 @@ class Search2mzIdentML
           dataCollection(xml)
         }
     end
-    
+
     @pepxml.display_missed_mappings
     File.open(base_file + ".mzid", 'w') {|io| io.puts builder.to_xml}
   end
-  
-  
+
+
   private
-  
+
   # Takes the input filename and the filetype type (tp)
   def base_file
     if @pepxml.type == "pepxml"
       @pepxml.file.chomp('.pep.xml')
     end
   end
-  
+
   # Depth 1. This method name, as well as all the other ones like it, are named after the sections they create.
   def cvList(xml)
     xml.cvList {
@@ -79,27 +79,27 @@ class Search2mzIdentML
       xml.cv(:id => "UO", :fullName => "UNIT-ONTOLOGY", :uri => "http://obo.cvs.sourceforge.net/*checkout*/obo/obo/ontology/phenotype/unit.obo")
     }
   end
-  
+
   # Depth 1
   def analysisSoftwareList(xml)
-  
+
     #No reference to wich software was used for each SIL, just list the software
     summparams = @pepxml.peptideIdentificationSummaryParams
-    
-    #combined_software_list = [] 
+
+    #combined_software_list = []
     #De momento solo tengo X!Tandem. En los 3 SIL. No tiene sentido hacer este combined software list
     #summparams.each do |s| # Software_list Per SIL (AtiO2, Elu1A, Elu2A)
     #  s.search_params_set.searchparams.software_list.each do |soft|
     #   combined_software_list << soft
     #  end
     #end
-  
+
     #combined_software_list.flatten!.uniq! #No software por SIL, sino global!
     softwareList = summparams[0].search_params_set.searchparams.software_list #[0] es el del primer SIL Ati02, me da igual , todos son X!Tandem
-    
+
     xml.AnalysisSoftwareList {
       #combined_software_list.each do |software_name|
-      softwareList.each do |software_name, version|  
+      softwareList.each do |software_name, version|
         xml.AnalysisSoftware(:id => software_name, :version => version) {
           #1 <ContactRole>
           if software_name == "X\\!Tandem"
@@ -113,17 +113,17 @@ class Search2mzIdentML
               xml.Role {
                 xml.cvParam(:accession => "MS:1001267", :name => "software vendor",  :cvRef => "PSI-MS")
               }
-            }            
+            }
           elsif software_name == "pepXML_protXML_2_mzIdentML"
             #ContactRole no es obligatorio : minOccurs:0. Pero creo que es necesario para MIAPE compliance en el validator
             xml.ContactRole(:contact_ref => "ORG_UCM") {
-              xml.Role { 
+              xml.Role {
                 xml.cvParam(:accession => "MS:1001267", :name => "software vendor",  :cvRef => "PSI-MS")
               }
-            }          
+            }
           end
           #2 <SoftwareName>
-          if @pepxml.findAccession(software_name)[0] != "" 
+          if @pepxml.findAccession(software_name)[0] != ""
             xml.SoftwareName {
               array = @pepxml.findAccession(software_name)
               xml.cvParam(:accession => array[0], :name => array[1], :cvRef => "PSI-MS")
@@ -137,11 +137,11 @@ class Search2mzIdentML
           xml.Customizations "k-score scoring algorithm" if software_name == "X\\!Tandem"
         }
       end
-    }   
-    
+    }
+
   end
-  
-  
+
+
   # Depth 1
   def provider(xml)
     xml.Provider(:analysisSoftware_ref => "pepXML_protXML_2_mzIdentML", :id => "PROVIDER") {
@@ -152,7 +152,7 @@ class Search2mzIdentML
       }
     }
   end
-  
+
   #Depth 1
   def auditCollection(xml)
     xml.AuditCollection {
@@ -171,7 +171,7 @@ class Search2mzIdentML
       }
     }
   end
-  
+
   # Depth 1
   def sequenceCollection(xml)
     xml.SequenceCollection {
@@ -180,11 +180,11 @@ class Search2mzIdentML
       peptideEvidences(xml)
     }
   end
-  
+
   # Depth 2
   def dBSequences(xml)
     puts "  get proteins from pepxml object, method proteins and write <db_sequence>s "
-    proteins = @pepxml.proteins    
+    proteins = @pepxml.proteins
     proteins.each do |protein|
       xml.DBSequence(:id => protein[2], :searchDatabase_ref => "SDB_1", :accession => protein[0]) {
         xml.cvParam(:accession => "MS:1001088", :name => "protein description", :cvRef => "PSI-MS", :value => protein[1])
@@ -196,16 +196,16 @@ class Search2mzIdentML
     puts "done\n"
     puts "Time elapsed: #{this_time - @@beginning_time} seconds\n\n"
   end
-  
+
   # Depth 2
   def peptides(xml)
     puts "get peptides from pepxml object, method peptides and write <peptides>"
-    peptides = @pepxml.peptides    
+    peptides = @pepxml.peptides
     peptides.each do |peptide|
       pep_id = peptide[0]
       pep_seq, modif_arr = peptide[1][0], peptide[1][1]
       xml.Peptide(:id => pep_id) {
-        xml.PeptideSequence  pep_seq 
+        xml.PeptideSequence  pep_seq
         if !modif_arr.empty?
           modif_arr.each do |modif|
             cvParam = modif[-1]
@@ -228,10 +228,10 @@ class Search2mzIdentML
       xml.PeptideEvidence(
       :dBSequence_ref => "SDB_1_#{ev[4]}",
       #:frame => "",
-      :id => "#{ev[0]}_SDB_1_#{ev[4]}_#{ev[5]}_#{ev[6]}", 
+      :id => "#{ev[0]}_SDB_1_#{ev[4]}_#{ev[5]}_#{ev[6]}",
       :isDecoy => ev[7],
       :name => "",
-      :peptide_ref => ev[0], 
+      :peptide_ref => ev[0],
       :start => ev[5],
       :end => ev[6],
       :pre => ev[1],
@@ -241,16 +241,16 @@ class Search2mzIdentML
     this_time = Time.now
     puts "done\n"
     puts "Time elapsed: #{(this_time - @@beginning_time) / 60} minutes\n\n"
-    
+
   end
 
-  
+
   # Depth 1
   def analysisCollection(xml)
     puts "get identification parameters and write 2 <AnalysisCollection>  <AnalysisProtocolCollection>"
 
     xml.AnalysisCollection {
-      #SI_ATiO2 - #SIP_ATiO2 - #SIL_ATiO2 - #SID_ATiO2 
+      #SI_ATiO2 - #SIP_ATiO2 - #SIL_ATiO2 - #SID_ATiO2
       @summparams = @pepxml.peptideIdentificationSummaryParams
       all_sil_s = []
       @summparams.each do |s|
@@ -269,7 +269,7 @@ class Search2mzIdentML
         }
       end
       #<ProteinDetection> -maxOccurs = 1 #Static id name is OK
-      #<ProteinDetectionList> (under <AnalysisData>) -maxOccurs = 1 
+      #<ProteinDetectionList> (under <AnalysisData>) -maxOccurs = 1
       #<ProteinDetectionProtocol> (under <AnalysisProtocolCollection>) -maxOccurs = 1   TOO ! Static ids! Cool! Easy!
       xml.ProteinDetection(:id => "PD_1", :proteinDetectionList_ref => "PDL_1", :proteinDetectionProtocol_ref => "PDP_1") {
         all_sil_s.each do |sil|
@@ -278,7 +278,7 @@ class Search2mzIdentML
       }
     }
   end
-  
+
   # Depth 1
   def analysisProtocolCollection(xml)
     xml.AnalysisProtocolCollection {
@@ -289,60 +289,60 @@ class Search2mzIdentML
     puts "done\n"
     puts "Time elapsed: #{(this_time - @@beginning_time) / 60} minutes\n\n"
   end
-  
+
   # Depth 2
   def SpectrumIdentificationProtocol(xml)
-    
+
     enz_i = 0 # !?! Can't repeat enzyme id ! though I should be able to (trypsin in the 3 different SIPs)
-    @summparams.each do |s| #x3 
+    @summparams.each do |s| #x3
       if s.ms_run_params_set.msrunparams.inputraw_path =~ /^\//
         inputraw = s.ms_run_params_set.msrunparams.inputraw_path.split("/")[-1]
       else
         inputraw = s.ms_run_params_set.msrunparams.inputraw_path
       end
       searched_mod_param_arrs = s.search_params_set.searchparams.searched_modif_Arr
-      additional_search_params = s.search_params_set.searchparams.additional_search_params      
+      additional_search_params = s.search_params_set.searchparams.additional_search_params
       #search_engine = s.search_params_set.searchparams.software_list[-1][0]
       search_engine = "X\\!Tandem"
       xml.SpectrumIdentificationProtocol(:id => "SIP_#{inputraw}", :analysisSoftware_ref => search_engine) {
         xml.SearchType { #support for ms-ms only #Don't know of any value other than "ms-ms search." Should probably fix this in the future. #MS:1001010 (de novo search), MS:1001031 (spectral library search), MS:1001081 (pmf search), MS:1001584 (combined pmf + ms-ms search)
           xml.cvParam(:accession => "MS:1001083", :name => "ms-ms search", :cvRef => "PSI-MS")
-        }     
+        }
         xml.AdditionalSearchParams {
           additional_search_params.each do |param|
             xml.userParam(:name => param[0].to_s, :value => param[1].to_s)
           end
         }
-        
+
         xml.ModificationParams {
-          searched_mod_param_arrs.each do |m_arr| 
-            cvParam = m_arr[4] 
-            #puts cvParam          
+          searched_mod_param_arrs.each do |m_arr|
+            cvParam = m_arr[4]
+            #puts cvParam
             xml.SearchModification(:residues => m_arr[0], :massDelta => m_arr[1], :fixedMod => m_arr[3]) {
               xml.cvParam(:cvRef => cvParam[0], :accession => cvParam[1], :name => cvParam[2])
             }
-          end    
-        }        
-        
+          end
+        }
+
         xml.Enzymes {
           xml.Enzyme(:id => "Enz_#{enz_i}", :cTermGain => "OH", :nTermGain => "H", :semiSpecific => "0"){
-            xml.SiteRegexp  "\<\![CDATA[(\?\<=[KR])(\?\!P)]]\>" 
+            xml.SiteRegexp  "\<\![CDATA[(\?\<=[KR])(\?\!P)]]\>"
             xml.EnzymeName{
-              xml.cvParam(:accession => "MS:1001251", :name => "Trypsin", :cvRef => "PSI-MS")            
+              xml.cvParam(:accession => "MS:1001251", :name => "Trypsin", :cvRef => "PSI-MS")
             }
 
-          }        
+          }
         }
         enz_i += 1
-        
+
         xml.FragmentTolerance {
           frag_tol = s.search_params_set.searchparams.frag_tol
           xml.cvParam(:accession => "MS:1001412", :name => "search tolerance plus value", :value => frag_tol, :cvRef => "PSI-MS")
           xml.cvParam(:accession => "MS:1001413", :name => "search tolerance minus value", :value => frag_tol, :cvRef => "PSI-MS")
         }
-        
+
         xml.ParentTolerance {
-          parent_tol = s.search_params_set.searchparams.parent_tol   
+          parent_tol = s.search_params_set.searchparams.parent_tol
           xml.cvParam(:accession => "MS:1001412", :name => "search tolerance plus value", :value => parent_tol, :cvRef => "PSI-MS")
           xml.cvParam(:accession => "MS:1001413", :name => "search tolerance minus value", :value => parent_tol, :cvRef => "PSI-MS")
         }
@@ -354,7 +354,7 @@ class Search2mzIdentML
             xml.cvParam(:accession => "MS:?", :name => "?", :cvRef => "PSI-MS", :value => @pepxml.threshold)
           end
         }
-            
+
       }
     end
   end
@@ -371,7 +371,7 @@ class Search2mzIdentML
     }
 
   end
-  
+
   # Depth 1
   def dataCollection(xml)
     xml.DataCollection {
@@ -382,7 +382,7 @@ class Search2mzIdentML
       }
     }
   end
-  
+
   # Depth 2
   def inputs(xml)
     xml.Inputs { #The database searched and the input file converted to mzIdentML
@@ -394,21 +394,21 @@ class Search2mzIdentML
           xml.userParam(:name => File.basename(@pepxml.database))
         }
       }
-      
-      @summparams.each do |s|      
-        manufacturer = s.ms_run_params_set.msrunparams.manufacturer    
+
+      @summparams.each do |s|
+        manufacturer = s.ms_run_params_set.msrunparams.manufacturer
         spectra_data_ref = s.sil.sub("SIL", "SID")
-        #<SpectraData> -minOccurs 1 
+        #<SpectraData> -minOccurs 1
         #location = s.search_params_set.searchparams.frag_tol
         location = s.ms_run_params_set.msrunparams.inputraw_path + s.ms_run_params_set.msrunparams.raw_data_file_type
         #MALLL!! :location no es eso! tengo que poner la location del mzML !!
         xml.SpectraData(:id => spectra_data_ref, :location => location ) {
           xml.SpectrumIDFormat {
-            if manufacturer.include? "Thermo" or manufacturer.include? "thermo" 
+            if manufacturer.include? "Thermo" or manufacturer.include? "thermo"
               xml.cvParam(:accession => "MS:1000768", :name => "Thermo nativeID format", :cvRef => "PSI-MS")
             end
-          }        
-        }        
+          }
+        }
       end
     }
   end
@@ -424,10 +424,10 @@ class Search2mzIdentML
     end
 
   end
-  
+
   # Depth 4
   def spectrumIdentificationResult(xml, sil)
-  
+
     puts "writing <spectrum_identification_results> for sil: #{sil}"
     results = @pepxml.results[sil]
     i = 1
@@ -438,7 +438,7 @@ class Search2mzIdentML
           ident = item.ident
           #siiID = "SII_#{i}_#{ident.id}"
           siiID = ident.sii_id
-          
+
           #Depth 5
           xml.SpectrumIdentificationItem(
             :id => siiID,
@@ -458,21 +458,21 @@ class Search2mzIdentML
     puts "done\n"
     puts "Time elapsed: #{(this_time - @@beginning_time) / 60} minutes\n\n"
   end
-  
+
   # Depth 6
   def spectrumIdentificationItemVals(xml, item, siiID)
     #OJO! Porque Peptide_ref(s) pueden ser varios por cada SII !!
     pep_id = item.ident.pep
-    
+
     pep_evidences = item.pepEvidences
-    
+
     pep_ev_refs = []
     pep_evidences.each do |ev|
       pep_ev_refs << "#{ev[0]}_SDB_1_#{ev[4]}_#{ev[5]}_#{ev[6]}"
     end
-        
+
     pep_ev_refs.each do |ev|
-      xml.PeptideEvidenceRef(:peptideEvidence_ref => ev) 
+      xml.PeptideEvidenceRef(:peptideEvidence_ref => ev)
     end
 
     cv_params_set, user_params_set = item.vals[0], item.vals[1]
@@ -484,10 +484,10 @@ class Search2mzIdentML
         xml.userParam(:name => userparam[0], :value => userparam[1]) unless userparam.nil?
       end
     end
-   
+
    #OJORR! En estos cvParams van los scores pero también otras cosas como number of matched / unmatched ions!!
   end
-  
+
 
   def proteinDetectionList(xml)
 
@@ -506,19 +506,20 @@ class Search2mzIdentML
     this_time = Time.now
     puts "done\n"
     puts "Time elapsed: #{(this_time - @@beginning_time)/60} minutes\n\n"
-  
-  end  
+
+  end
 
 
   def proteinHypothesis(xml, protein_hyp)
-   
+
     #1.upto protein_hyp.n_indist_proteins do
       xml.ProteinDetectionHypothesis(:id => protein_hyp.name, :passThreshold => protein_hyp.pass_threshold) {
         params = protein_hyp.vals
 
         #El problema es que si tengo indist_proteins > 1 para las siguientes a la primera solo tengo protein_name
-      
+
         protein_hyp.pep_hypothesis_set.each do |pep|
+          puts "WATCH OUT! no peptide hypotheses found for protein_hyp #{protein_hyp} !!" if protein_hyp.pep_hypothesis_set.empty?
           pep.pep_hypothesis_hash.each do |pep_ev, sii_s|
             xml.PeptideHypothesis(:peptideEvidence_ref => pep_ev) {
               sii_s.each do |sii_id|
@@ -529,9 +530,9 @@ class Search2mzIdentML
         end
         #cvParam id: MS:1001093 name: sequence coverage
         if  !params[3] == ""
-          xml.cvParam(:accession => "MS:1001093", :name => "sequence coverage", :cvRef => "PSI-MS", :value => params[3]) 
-        else 
-          xml.cvParam(:accession => "MS:1001093", :name => "sequence coverage", :cvRef => "PSI-MS", :value => "0") 
+          xml.cvParam(:accession => "MS:1001093", :name => "sequence coverage", :cvRef => "PSI-MS", :value => params[3])
+        else
+          xml.cvParam(:accession => "MS:1001093", :name => "sequence coverage", :cvRef => "PSI-MS", :value => "0")
         end
 
         xml.userParam(:name => "n_indist_proteins", :value => params[1]) unless params[1] == ""
@@ -543,20 +544,20 @@ class Search2mzIdentML
         xml.userParam(:name => "pct_spectrum_ids", :value => params[7]) unless params[7] == ""
         xml.userParam(:name => "confidence", :value => params[8]) unless params[8] == ""
         xml.userParam(:name => "prot_length", :value => params[9]) unless params[9] == ""
-      }        
-        
+      }
+
     #end
-      
+
   end
-     
+
 
 end
 
       #~ protein_hyp.pep_hypothesis_set.each do |pep|
         #~ pep.pep_evs.each do |pep_ev|
           #~ xml.PeptideHypothesis(:peptideEvidence_ref => pep_ev) {
-            #~ pep.sii_s.each do |sii_id| 
-              #~ xml.SpectrumIdentificationItemRef(:spectrumIdentificationItem_ref => sii_id) 
+            #~ pep.sii_s.each do |sii_id|
+              #~ xml.SpectrumIdentificationItemRef(:spectrumIdentificationItem_ref => sii_id)
               #~ #NORR!! <SpectrumIdentificationItemRef> no tiene subelemento userParam !!
               #~ #Quiza todo esto ponerlo mejor en <SpectrumIdentificationItem>
               #~ #params = pep.vals
@@ -573,6 +574,6 @@ end
             #~ end
           #~ }
         #~ end
-        #~ 
+        #~
       #~ end
-      
+
